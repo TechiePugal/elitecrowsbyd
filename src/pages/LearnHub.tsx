d3bed8210998e842665b331b2f55df90ff0eec46
Techie Pugal <elitecrowsindia@gmail.com>
2026-06-13 01:15:10 +0530

    floatingbutupdate

--- src/pages/LearnHub.tsx
+++ src/pages/LearnHub.tsx
@@ -588,7 +588,7 @@ Submitted via LearnHub Page
             whileTap={{ scale: 0.95 }}
             style={{
               position: 'fixed',
-              right: '340px',
+              right: '330px',
               bottom: '40px',
               zIndex: 999,
               width: '50px',
